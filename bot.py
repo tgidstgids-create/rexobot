1dfce62faae6d490c9c042c84ba1aa9e0514c371
tgidstgids-create <tgidstgids@gmail.com>
2026-02-16 22:24:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ from pyrogram.errors import (
 # -----------------------
 BOT_TOKEN = os.getenv('BOT_TOKEN', '7945581080:AAGyFI97QRnwqN26kQuNOoIz1JqCA-AYQbg')
 ADMIN_ID = int(os.getenv('ADMIN_ID', '8043091004'))
-MONGO_URL = os.getenv('MONGO_URL', 'mongodb+srv://<db_username>:<db_password>@tgwpidrobot.gqw5lxp.mongodb.net/?appName=Tgwpidrobot')
+MONGO_URL = os.getenv('MONGO_URL'mongodb+srv://Rexootpbot:tuSJ61tCHDM1jZAP@otpbot.hwmknyh.mongodb.net/?appName=Otpbot')
 API_ID = int(os.getenv('API_ID', '30038466'))
 API_HASH = os.getenv('API_HASH', '5a492a0dfb22b1a0b7caacbf90cbf96e')
 
